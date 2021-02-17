3f0bb736a6bf4b49bbb7ce37db35df617c500698
DrVonDevious <drvondevious@gmail.com>
2021-02-16 22:39:41 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>The Devious Dev</title>
+        <p>
+          Hello, World.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

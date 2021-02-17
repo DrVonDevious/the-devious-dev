ecdb456e5e2c82b947b8a3ea060c7a1d1080743f
DrVonDevious <drvondevious@gmail.com>
2021-02-16 22:57:06 -0600

    fix index.html errorOC

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <title>The Devious Dev</title>
         <p>
           Hello, World.
         </p>
